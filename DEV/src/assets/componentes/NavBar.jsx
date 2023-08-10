28cb76173d726e53996738f909df50023490d00a
Luis-Castro1 <129408146+Luis-Castro1@users.noreply.github.com>
2023-08-10 03:11:00 +0000

    NavBar

--- DEV/src/assets/componentes/NavBar.jsx
+++ DEV/src/assets/componentes/NavBar.jsx
@@ -1,13 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faMagnifyingGlass, faUser, faHeart, faCartShopping, faBars }
+import { faMagnifyingGlass, faUser, faHeart, faCartShopping }
   from "@fortawesome/free-solid-svg-icons"
+
+  const classLi = "list-none text-black font-normal rounded-md px-1 py-1 text-sm"
+
 
 export const NavBar = () => {
   return (
-    <div className="grid md:grid-cols-3  grid-cols-1 justify-between">
+    <div className="grid md:grid-cols-3  grid-cols-1 justify-between py-1">
 
       <div className=" md:block px-3 w-full">
-        <div className="flex ml-2 pt-2 items-center relative">
+        <div className="flex ml-2 py-1 my-2 items-center relative">
           <FontAwesomeIcon className="absolute pt-[10px] pr-0 pb-[10px] sm:ml-0 ml-[45px] sm:pl-[13px] z-[100] text-[20px]" icon={faMagnifyingGlass} style={{ color: "#0F172A", }} />
 
           <input className="ml-[40px] pl-[30px] sm:ml-0 sm:pl-[40px] text-[16px] w-[75%] border-b-[1px] border-stone-900 bg-transparent focus:outline-none focus:border-sky-800" type="search" placeholder="Buscar productos" id="search-text" />
@@ -17,37 +20,37 @@ export const NavBar = () => {
 
 
 
-      <div className="bg-amber-500 justify-center flex w-full">
+      <div className="justify-center flex w-full">
         <div className="mt-4 mb-2 pt-1">
-          <img src="/src/assets/imagenes/Logo-dev.png" alt="Logo" className="w-[200px]" />
+          <img src="/src/assets/imagenes/Logo-dev.png" alt="Logo" className="w-[210px] hover:scale-105 cursor-pointer transition-all duration-200" />
         </div>
       </div>
 
 
 
-      <div className="flex justify-between items-center bg-green-600">
+      <div className="grid grid-cols-3 justify-center items-start my-2 py-1">
 
-        <ul className='hidden  sm:flex text-[18px]
-      sm:w-[438px] sm:place-content-around sm:text-[16px] sm:items-center'>
-
-          <li>
-            <a className="  text-white rounded-md px-2 py-2 text-sm font-medium" href="#">Cuenta</a> <FontAwesomeIcon icon={faUser} style={{ color: "#ededed", }} />
+        <div className='justify-center flex'>
+          <li className={classLi}>
+            <a className="  " href="#">Cuenta</a> <FontAwesomeIcon icon={faUser} style={{ color: "black", }} />
           </li>
+          </div>
 
-          <li>
-            <a className=" text-white rounded-md px-2 py-2 text-sm font-medium" href="#">Favoritos</a> <FontAwesomeIcon icon={faHeart} style={{ color: "#ededed", }} />
+          <div className='justify-center flex'>
+          <li className={classLi}>
+            <a  href="#">Favoritos</a> <FontAwesomeIcon icon={faHeart} style={{ color: "black", }} />
           </li>
+          </div>
 
-          <li>
-            <a className=" text-white rounded-md px-2 py-2 text-sm font-medium" href="#">Carrito</a> <FontAwesomeIcon icon={faCartShopping} style={{ color: "#ededed", }} />
+          <div className='justify-center flex'>
+          <li className={classLi}>
+            <a className="" href="#">Carrito</a> <FontAwesomeIcon icon={faCartShopping} style={{ color: "black", }} />
           </li>
-        </ul>
-
-        <div className='w-10 h-4 cursor-pointer sm:hidden'>
-          <FontAwesomeIcon icon={faBars} style={{ color: "#0f172a", fontSize: 16 }} />
         </div>
 
+
       </div>
+
     </div>
   )
 }
