2483891a7cbd32d6cd12d45cde95597b30d922dc
Luis-Castro1 <129408146+Luis-Castro1@users.noreply.github.com>
2023-08-14 20:34:07 +0000

    todo ok

--- DEV/src/assets/data/categorias.js
+++ DEV/src/assets/data/categorias.js
@@ -3,17 +3,17 @@ const categorias = [
     {
         id: 1,
         name: "HOMBRE",
-        image: "src/assets/imagenes/Hombre.png"
+        image: "/src/assets/imagenes/Hombre.png"
     },
     {
         id: 2,
         name: "MUJER",
-        image: "src/assets/imagenes/Mujer.png"
+        image: "/src/assets/imagenes/Mujer.png"
     },
     {
         id: 3,
         name: "NIÑO",
-        image: "src/assets/imagenes/Niño.png"
+        image: "/src/assets/imagenes/Niño.png"
     },
 ]
 export default categorias;
