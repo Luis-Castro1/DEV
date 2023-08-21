4ef28da608ae67346a70a21ebfcec1495386959b
Luis-Castro1 <129408146+Luis-Castro1@users.noreply.github.com>
2023-08-21 22:11:16 +0000

    list product

--- DEV/src/assets/componentes/Products.jsx
+++ DEV/src/assets/componentes/Products.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import CategoriasNav from '../componentes/CategoriasNav'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+
+
 export const Products = ({ products }) => {
 
 
@@ -9,23 +13,28 @@ export const Products = ({ products }) => {
 
       <CategoriasNav />
 
-      <div className='grid  grid-cols-5 mx-5'>
+      <div className='sm:grid flex flex-wrap  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-5'>
 
         {products.map((product) => (
 
-          <div className='bg-white w-full'>
-            <div key={product.id} className="bg-red-500 py-[8rem] mx-5 my-5 grid justify-center gap-y-3">
-              <img src={product.img} alt="" />
-
-              <div className="">
-                <strong>{product.name}</strong>
+          <div className='max-w-[370px] mx-auto'>
+            <div key={product.id} className=" bg-white  shadow-sm shadow-slate-950 relative overflow-hidden rounded-[20px] cursor-pointer font-darker-grotesque font-normal py-[1px] mx-2 my-5 grid justify-center gap-y-1">
+              
+              <div className="flex justify-center overflow-hidden bg-white">
+              <img className='w-[370px] h-[370px]  rounded-[20px] hover:scale-125 transition-all duration-300 ' src={product.img} alt="" />
               </div>
-              <div className="">
+
+              <div className="items-center text-white flex justify-between absolute inset-0 top-[325px] h-[3rem] bg-black/80  px-[2px] ">
+                <strong>{product.name}</strong>
+            
+              
                 <h3>{product.category}</h3>
               </div>
 
-              <div>
-                <button>Agregar al carro</button>
+              <div className='flex justify-center'>
+                <button className='w-full bg-white rounded-[2px]'>Agregar al carro  
+                <FontAwesomeIcon icon={faCartPlus} /></button>
+               
               </div>
             </div>
           </div>
--- DEV/src/assets/componentes/Recomendation.jsx
+++ DEV/src/assets/componentes/Recomendation.jsx
@@ -14,7 +14,7 @@ export const Recomendation = () => {
 
                 <div className="relative overflow-hidden rounded-[20px] cursor-pointer font-darker-grotesque font-normal">
                     <div className=' flex justify-center overflow-hidden'>
-                        <img className=" w-[310px] h-[370px] mt-[15px] rounded-[20px] hover:scale-110 transition-all duration-500 " src={p.img} alt={p.title} />
+                        <img className=" w-[310px] h-[370px] mt-[15px] rounded-[20px] hover:scale-125 transition-all duration-300 " src={p.img} alt={p.title} />
                     </div>
 
                     <div className="items-center text-white flex justify-between absolute inset-0 top-[21rem] h-[3rem] bg-black/80  px-[2px]  ">
