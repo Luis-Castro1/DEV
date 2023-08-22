f97ec2cac9404de564086172f715e85a1aad404f
Luis-Castro1 <129408146+Luis-Castro1@users.noreply.github.com>
2023-08-22 07:31:57 +0000

    slider

--- DEV/src/assets/componentes/Slider.jsx
+++ DEV/src/assets/componentes/Slider.jsx
@@ -1,22 +1,73 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faChevronLeft, faChevronRight, faCircle } from "@fortawesome/free-solid-svg-icons"
+import { faChevronLeft as Left, faChevronRight as Right, faCircle } from "@fortawesome/free-solid-svg-icons"
 import sliderimg from "../data/sliderimg"
 import { useState } from "react"
+import { Link } from 'react-router-dom'
+
 
 
 export const Slider = () => {
 
+    const styleBottom = () =>{
+        name => 'Left' ? ""
+    }
+
+
     return (
-       <div className="Contenedor padre">
+        <div className="Contenedor padre relative overflow-hidden max-w-[1480px] mx-[20px] my-auto">
 
-            <div className="contenedor slider">
+            <div className="container-slide flex flex-nowrap ">
+
+                <div className="slider md:relative min-w-full overflow-hidden ease-in-out duration-300 z-10 max-h-[800px]">
+
+                    <Link to="/" >
+                        <img className="w-full align-top " src="src/assets/imagenes/Slider1.jpg" alt="" />
+                    </Link>
+
+                    <div className="relative bg-black md:absolute md:bottom-[4rem] min-[870px]:bottom-[4rem] lg:bottom-12 min-[1170px]:bottom-[1rem] md:bg-black/50 text-white w-full py-[10px] px-[60px] text-center">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </div>
+
+                </div>
+                <div className="slider relative min-w-full overflow-hidden ease-in-out duration-300 z-10 max-h-[900px]">
+
+                    <Link to="/" >
+                        <img className="w-full align-top " src="src/assets/imagenes/Slider2.jpg" alt="" />
+                    </Link>
+
+                    <div className="bg-black/50 text-white w-full py-[10px] px-[60px] text-center">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quae officiis, at et deserunt aperiam?</p>
+                    </div>
+
+                </div>
+                <div className="slider relative min-w-full overflow-hidden ease-in-out duration-300 z-10 max-h-[900px]">
+
+                    <Link to="/" >
+                        <img className="w-full align-top " src="src/assets/imagenes/Slider3.jpg" alt="" />
+                    </Link>
+
+                    <div className="bg-black/50 text-white w-full py-[10px] px-[60px] text-center">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quae officiis, at et deserunt aperiam?</p>
+                    </div>
+
+                </div>
+
+
+
 
             </div>
 
-            <div className="Controles">
-                
+            <div className="Controles absolute top-0 z-20 w-full h-full pointer-events-none">
+
+                <button name="Left" className="pointer-events-all bg-white/50 border-none cursor-pointer outline-none w-[50px] h-full text-center absolute ease-in-out duration-300">
+                    <FontAwesomeIcon className="text-[50px] left-0" icon={Left}> </FontAwesomeIcon>
+                </button>
+                <button className="pointer-events-all bg-white/50 border-none cursor-pointer outline-none w-[50px] h-full text-center absolute ease-in-out duration-300 right-0">
+                    <FontAwesomeIcon className="text-[50px]" icon={Right}> </FontAwesomeIcon>
+                </button>
+
             </div>
 
-       </div> 
+        </div >
     )
 }
--- DEV/src/assets/data/sliderimg.js
+++ DEV/src/assets/data/sliderimg.js
@@ -1,12 +1,15 @@
 const sliderimg= [
     {
-        url:"https://phantom-telva.unidadeditorial.es/734dbecbf6d5f492ebcceb8a08b8bdd2/resize/828/f/jpg/assets/multimedia/imagenes/2022/08/31/16619586981133.jpg"
+        id: 1,
+        url:"DEV/src/assets/imagenes/Slider1.jpg"
     },
     {
-        url:"https://images.pexels.com/photos/17738037/pexels-photo-17738037/free-photo-of-moda-hombre-relajacion-joven.jpeg"
+        id: 2,
+        url:"DEV/src/assets/imagenes/Slider2.jpg"
     },
     {
-        url:"https://www.marcasmadeinspain.es/wp-content/uploads/2023/05/10-camisas-de-Zara-que-semejan-de-Massimo-Dutti-%C2%A1lujo-800x440.jpg"
+        id: 3,
+        url:"DEV/src/assets/imagenes/Slider3.jpg"
     },
 ];
 
