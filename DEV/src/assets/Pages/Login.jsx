a2636f806e4426020efcb6edab3c3fa7c24ea575
betoo011 <cantilloalberto39@gmail.com>
2023-08-24 20:28:53 -0500

    cambio

--- DEV/src/assets/Pages/Login.jsx
+++ DEV/src/assets/Pages/Login.jsx
@@ -6,9 +6,9 @@ export const Login = () => {
 
   
   return (
-    <div className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-400 bg-white rounder-10'>
-     <form action="" className='px-20 box-border'>
-<img src="src/assets/imagenes/Logo-dev.png" alt="" className='"w-90 h-12 block mx-auto -my-6' />
+    <div className='flex flex-col justify-center items-center'>
+  <div className='h-96 w-96 bg-white rounded-2xl'>
+  <img src="src/assets/imagenes/Logo-dev.png" alt="" className='"w-90 h-12 block mx-auto -my-6' />
 <h1 className='text-center pb-20 font-Darker-Grotesque text-1.5rem text-black mt-11'>ACCEDE A TU CUENTA </h1>
 
 <div className='E-MAIL'>
@@ -22,8 +22,8 @@ export const Login = () => {
 <FontAwesomeIcon icon={faKey} />
 <input type="password" />
 </div>
+  </div>
 
-     </form>
      </div>
     
 
