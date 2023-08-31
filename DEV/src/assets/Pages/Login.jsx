ab526dd67e7cc9317095d73e5242b752bd8c70a7
betoo011 <cantilloalberto39@gmail.com>
2023-08-30 19:02:41 -0500

    login terminado

--- DEV/src/assets/Pages/Login.jsx
+++ DEV/src/assets/Pages/Login.jsx
@@ -2,49 +2,52 @@ import './login.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLock, faUser } from '@fortawesome/free-solid-svg-icons'
 import React, { useEffect } from 'react';
+
 export const Login = () => {
+  useEffect(() => {
+    const emailInput = document.getElementById('email_input');
+    const passwordInput = document.getElementById('pass_input');
+  
+    const handleInput = event => {
+      const input = event.target;
+      const label = input.nextElementSibling;
+      
+      if (input.value) {
+        input.classList.add('border-gray-400', 'border-b-2');
+        label.classList.add('-top-6');
+      } else {
+        input.classList.remove('border-gray-400', 'border-b-2');
+        label.classList.remove('-top-6');
+      }
+    };
 
+    emailInput.addEventListener('input', handleInput);
+    passwordInput.addEventListener('input', handleInput);
 
-  const Login = () => {
-    useEffect(() => {
-      const input = document.getElementById('email');
-      const label = document.getElementById('email_label');
-  
-      const handleInput = event => {
-        if (event.target.value) {
-          ['border-gray-400', 'border-b-2'].forEach(c => input.classList.add(c));
-          ['top-14'].forEach(c => label.classList.add(c));
-        } else {
-          ['border-gray-400', 'border-b-2'].forEach(c => input.classList.remove(c));
-          ['top-14'].forEach(c => label.classList.remove(c));
-        }
-      };
-  
-      input.addEventListener('input', handleInput);
-  
-      return () => {
-        input.removeEventListener('input', handleInput);
-      };
-    }, []);
+    return () => {
+      emailInput.removeEventListener('input', handleInput);
+      passwordInput.removeEventListener('input', handleInput);
+    };
+  }, []); 
+
   return (
-
     <div className='flex justify-center items-center h-screen'>
       <div className='h-[420px] w-[400px] bg-white rounded-2xl relative'>
         <img src="src/assets/imagenes/Logo-dev.png" alt="" className="w-45 h-12 mx-auto absolute left-1/2 -translate-x-1/2 -translate-y-1/2" />
 
         <form className='h-full flex flex-col pt-6 items-center'>
-          <h1 className=" mt-9 text-[1.5rem] font-darker-grotesque text-black bg">ACCEDE A TU CUENTA </h1>
+          <h1 className=" mt-9 text-[1.5rem] font-darker-grotesque text-black bg">ACCEDE A TU CUENTA</h1>
 
-          <div className='pt-20 relative font-sans left-[-10px]'>
+          <div className=' mt-20 relative font-sans left-[-10px]'>
             <FontAwesomeIcon icon={faUser} className='absolute left-[-20px] leading-[30px]'/>
-            <input type="text" id="email" className='w-[300px] focus:outline-none border-b-[2px] text-base focus:border-gray-400 transition-colors peer'/>
-          <label htmlFor="email" id='email_label' className='absolute left-0 text-gray-400 text-[13px] cursor-text peer-focus:top-14 ease-in-out duration-500'>E-MAIL</label>
+            <input type="text" id="email_input" className='w-[300px] focus:outline-none border-b-[2px] text-base focus:border-gray-500 transition-colors peer'/>
+          <label htmlFor="email" id='email_label' className='absolute left-0 text-gray-400 text-[13px] transition-all duration-300  pointer-events-none peer-focus:-top-6'>E-MAIL</label>
           </div>
 
-          <div className='pt-10 relative font-sans left-[-10px]'>
+          <div className='mt-10 relative font-sans left-[-10px]'>
             <FontAwesomeIcon icon={faLock} className='absolute left-[-20px] leading-[30px]' />
-            <input type="password" id='contraseña' className='w-[300px] focus:outline-none border-b-[2px] text-base focus:border-gray-400 ease-in-out-colors peer' />
-            <label htmlFor="contraseña" className='absolute left-0 text-gray-400 text-[13px] cursor-text peer-focus:top-4 ease-in-out duration-500'>CONTRASEÑA</label>
+            <input type="password" id="pass_input" className='w-[300px] focus:outline-none border-b-[2px] text-base focus:border-gray-500 transition-colors peer' />
+            <label htmlFor="contraseña" id='pass_label' className='absolute left-0 text-gray-400 text-[13px] transition-all pointer-events-none peer-focus:-top-6'>CONTRASEÑA</label>
           </div>
 
           <div className='flex items-center flex-col pt-10 font-darker-grotesque  '>
@@ -64,4 +67,6 @@ export const Login = () => {
 
 
   )
-}} 
+
+}
+
