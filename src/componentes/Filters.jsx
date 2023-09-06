15ec68316e2e665faf695168ad30ca2aaf4a637f
Luis-Castro1 <luisangelcastrocabrera@gmail.com>
2023-09-05 23:36:16 -0500

    f

--- src/componentes/Filters.jsx
+++ src/componentes/Filters.jsx
@@ -1,43 +1,61 @@
-import { useState, useId } from "react"
+import { useState, useId, useEffect } from "react"
 
+export function Filters({ onChange }) {
 
-export function Filters({onChange}) {
-
-    const [isSelect, setIsSelect] = useState(true);
-
+    const [isGrabbing, setIsGrabbing] = useState(false); // Estado para controlar el estilo del cursor
     const [minPrice, setMinPrice] = useState(0);
     const minPriceFilterdId = useId();
 
-    const handleChangeMinPrice = (e) =>{
-  
+    const handleChangeMinPrice = (e) => {
+
+
         setMinPrice(e.target.value)
-        onChange (prevState =>({
+        onChange(prevState => ({
             ...prevState,
             minPrice: e.target.value
         }))
     }
 
-    const handleChangeIsSelect = (e) =>{
-        addEventListener('click', setIsSelect(!isSelect));
-    }
+    useEffect(() => {
+        const price = document.getElementById(minPriceFilterdId);
+
+        const handleMouseDown = () => {
+            setIsGrabbing(true);
+        };
+
+        const handleMouseUp = () => {
+            setIsGrabbing(false);
+        };
+
+        if (price) {
+            price.addEventListener("mousedown", handleMouseDown);
+            document.addEventListener("mouseup", handleMouseUp);
+
+            return () => {
+                price.removeEventListener("mousedown", handleMouseDown);
+                document.removeEventListener("mouseup", handleMouseUp);
+            };
+        }
+    }, [minPriceFilterdId]);
 
     return (
 
         <>
             <section>
                 <div className="flex gap-4 mx-8 items-center mt-[1rem]">
-                    <label className="pointer-events-none" htmlFor={minPriceFilterdId}>Precio minimo</label>
-                    <input
-                        className={isSelect? "cursor-grab" : "cursor-grabbing"}
-                        type="range"
-                        id={minPriceFilterdId}
-                        min= '0'
-                        max= '3000'
-                        onChange={handleChangeMinPrice}
-                        onClickCapture={handleChangeIsSelect}
-                        
-                    />
-                    <span>{minPrice}</span>
+                    <div className="flex gap-4">
+                        <label className="pointer-events-none" htmlFor={minPriceFilterdId}>Precio minimo</label>
+                        <input
+                            className={`${isGrabbing ? "cursor-grabbing" : "cursor-grab"} inputClass`}
+                            type="range"
+                            id={minPriceFilterdId}
+                            min='0'
+                            max='3000'
+                            onChange={handleChangeMinPrice}
+
+                        />
+                        <span>{minPrice}</span>
+                    </div>
                 </div>
             </section>
         </>
