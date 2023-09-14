15b3d0aa5c70039f00a1cae2a97ffdef41654c3d
Luis-Castro1 <luisangelcastrocabrera@gmail.com>
2023-09-14 12:10:19 -0500

    validación del login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLock, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLock, faUser, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons'
 import { useAuth } from '../context/AuthProvider';
 
 export const Login = () => {
@@ -15,6 +15,7 @@ export const Login = () => {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const emailInput = document.getElementById('email_input');
@@ -42,11 +43,23 @@ export const Login = () => {
     };
   }, []);
 
+  const submitInfo = (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      console.log("error")
+      setError(true)
+    } else {
+      console.log("listo")
+      setError(false)
+    }
+
+  }
 
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className='h-[420px] w-[400px] bg-white rounded-2xl relative'>
+      <div className='h-auto w-[400px] bg-white rounded-2xl relative'>
         <img src="src/assets/imagenes/Logo-dev.png" alt="" className="w-45 h-12 mx-auto absolute left-1/2 -translate-x-1/2 -translate-y-1/2" />
 
         <form className='h-full flex flex-col pt-6 items-center'>
@@ -80,13 +93,32 @@ export const Login = () => {
             <label htmlFor="contraseña" id='pass_label' className='absolute left-0 text-gray-400 text-[13px] transition-all duration-500 pointer-events-none peer-focus:-translate-y-5'>CONTRASEÑA</label>
           </div>
 
-          <div className='flex items-center flex-col pt-10 font-darker-grotesque  '>
-            <input type="button" value="INICIAR SESION" className='py-1 pb-1 block mb-2 w-[290%] cursor-pointer border-[1px] border-black bg-gray-200 hover:bg-gray-300 font-semibold text-sm ' />
-            <Link to='/register' className='py-1 pb-1 block mb-2 w-[290%] cursor-pointer border-[1px] border-black bg-gray-200 hover:bg-gray-300 font-semibold text-sm text-center'>
-              <button className='' type="button" >REGISTRATE
-              </button>
+          {error && (
+           
+              <div className='flex text-red-600 mt-5'>
+                <p className='text-[14px] font-sans'>
+                <FontAwesomeIcon icon={faTriangleExclamation} className='ml-2' />
+                <b className='ml-2'>Error:</b> Por favor rellene los campos correctamente.
+              </p>
+              </div>
+           
+          )}
 
+          <div className={`flex items-center flex-col ${!error ? 'pt-10' : 'mt-5'} font-darker-grotesque `}>
+
+            <input
+              onClick={submitInfo}
+              type="button"
+              value="INICIAR SESION"
+              className='py-1 pb-1 block mb-2 w-[290%] cursor-pointer border-[1px] border-black bg-gray-200 hover:bg-gray-300 font-semibold text-sm ' />
+
+            <Link to='/register' className='py-1 pb-1 block mb-2 w-[290%] cursor-pointer border-[1px] border-black bg-gray-200 hover:bg-gray-300 font-semibold text-sm text-center'>
+              <button
+                type="button"
+              >REGISTRATE
+              </button>
             </Link>
+
           </div>
 
           <div className='relative left-[-55px] font-sans'>
